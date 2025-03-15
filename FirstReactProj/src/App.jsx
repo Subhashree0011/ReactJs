3b18cd8d29455eb8442b5f18e57fe335bbeebf8a
Subhashree001 <shree.subha.b07@gmail.com>
2025-03-15 23:49:14 +0530

    change the border roundedness to 3xl using tailwind css

--- FirstReactProj/src/App.jsx
+++ FirstReactProj/src/App.jsx
@@ -18,26 +18,38 @@ function App() {
    <>
    <h1 className="bg-blue-800  p-4"  >Coding is important</h1>
 
-   <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" width="384" height="512"></img>
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+    <figure class="md:flex bg-slate-100 rounded-3xl p-8 md:p-0 dark:bg-slate-800 mt-10 ml-4 mr-4">
+
+    <img class="w-24 h-24 md:w-48 md:h-auto  rounded-3xl mx-auto" src="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" width="384" height="512"></img>
+    
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+
+        <blockquote>
+
+          <p class="text-lg font-medium">
+            “Tailwind CSS is the only framework that I've seen scale
+            on large teams. It’s easy to customize, adapts to any design,
+            and the build size is tiny.”
+          </p>
+
+        </blockquote>
+{/* 
+        <figcaption class="font-medium">
+
+          <div class="text-sky-500 dark:text-sky-400">
+
+            Sarah Dayan
+
+          </div>
+
+          <div class="text-slate-700 dark:text-slate-500">
+            Staff Engineer, Algolia
+          </div>
+
+        </figcaption> */}
+
       </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+  </figure>
 
 
 
@@ -48,8 +60,8 @@ function App() {
    <button>Remove value</button> */}
 
 
-   <Card username ="Subhashree"/>
-   <Card/>
+   {/* <Card username ="Subhashree"/>
+   <Card/> */}
 
 
 
